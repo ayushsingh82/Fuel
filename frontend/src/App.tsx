99de044711edf957b561bffb797eed51f498e3c2
shanrasool <28012006rasool@gmail.com>
2024-03-31 00:46:14 +0530

    added a button for home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import { useConnectUI, useIsConnected, useWallet } from "@fuels/react";
 import { ContractAbi__factory } from "./contracts";
 import AllItems from "./components/AllItems";
 import ListItem from "./components/ListItem";
+import Home from "./components/Home";
 import "./App.css";
 
 const CONTRACT_ID =
   "0x3b598fc9103ce3c5c7a29663aee51099b3374958ba1016280caf802fdeb5aad8";
 
 function App() {
-  const [active, setActive] = useState<"all-items" | "list-item">("all-items");
+  const [active, setActive] = useState<"all-items" | "list-item" | "home">(
+    "home"
+  );
   const { isConnected } = useIsConnected();
   const { connect, isConnecting } = useConnectUI();
   const { wallet } = useWallet();
@@ -29,6 +32,13 @@ function App() {
       </header>
       <nav>
         <ul>
+          <li
+            className={active === "home" ? "active-tab" : ""}
+            onClick={() => setActive("home")}
+            style={{ padding: "4px 12px", borderRadius: "16px" }}
+          >
+            Home
+          </li>
           <li
             className={active === "all-items" ? "active-tab" : ""}
             onClick={() => setActive("all-items")}
@@ -50,6 +60,7 @@ function App() {
           <div>
             {active === "all-items" && <AllItems contract={contract} />}
             {active === "list-item" && <ListItem contract={contract} />}
+            {active === "home" && <Home />}
           </div>
         ) : (
           <div>
