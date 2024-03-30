3e7b2bec0f912b05ea87370e1c4dbd5b0de41271
shanrasool <28012006rasool@gmail.com>
2024-03-30 20:59:04 +0530

    added the loader

--- frontend/src/components/AllItems.tsx
+++ frontend/src/components/AllItems.tsx
@@ -83,7 +83,18 @@ export default function AllItems({ contract }: AllItemsProps) {
       {status === "error" && (
         <div>Something went wrong, try reloading the page.</div>
       )}
-      {status === "loading" && <div>Loading...</div>}
+      {status === "loading" && (
+        <div className="pt-16">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-brandGreen border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
